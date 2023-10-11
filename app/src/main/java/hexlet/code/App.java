e4e8dddbafee7d875fe012d752da3644208bd442
SickJoke282 <menistr2@DESKTOP-VVJN9FC.localdomain>
2023-10-11 15:36:45 +0500

    add a default javalin application

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,2 +1,19 @@
-package hexlet.code;public class App {
+package hexlet.code;
+
+import io.javalin.Javalin;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+        app.get("/", ctx -> ctx.result("Hello, World!"));
+        return app;
+    }
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(8080);
+    }
 }
