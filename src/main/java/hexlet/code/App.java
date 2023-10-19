b70fb08d281b8dbfb4015fad0d4fbdb3abc2c795
SickJoke282 <menistr2@DESKTOP-VVJN9FC.localdomain>
2023-10-19 17:45:05 +0500

    added a temp engine for jte templates

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,18 +2,29 @@ package hexlet.code;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import gg.jte.resolve.ResourceCodeResolver;
+import hexlet.code.dto.BasePage;
 import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
 import lombok.extern.slf4j.Slf4j;
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.sql.SQLException;
+import java.util.Collections;
 import java.util.stream.Collectors;
 
 @Slf4j
 public class App {
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        return TemplateEngine.create(codeResolver, ContentType.Html);
+    }
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "7070");
         return Integer.valueOf(port);
@@ -34,10 +45,13 @@ public class App {
             statement.execute(sql);
         }
         BaseRepository.dataSource = dataSource;
-        Javalin app = Javalin.create(config -> {
-            config.plugins.enableDevLogging();
+        Javalin app = Javalin.create(config -> config.plugins.enableDevLogging());
+        JavalinJte.init(createTemplateEngine());
+
+        app.get("/", ctx -> {
+            var page = new BasePage("Hello, World!");
+            ctx.render("index.jte", Collections.singletonMap("page", page));
         });
-        app.get("/", ctx -> ctx.result("Hello, World!"));
         return app;
     }
     public static void main(String[] args) throws SQLException, IOException {
