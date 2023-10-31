76e34c0f8c94ca773ab62373a627c8c88ad99dc4
SickJoke282 <menistr2@DESKTOP-VVJN9FC.localdomain>
2023-10-31 16:07:33 +0500

    added more test cases

--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -3,7 +3,12 @@ package hexlet.code;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+import hexlet.code.model.Url;
+import hexlet.code.repository.UrlRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -41,8 +46,35 @@ public class AppTest {
         JavalinTest.test(app, (server, client) -> {
             var requestBody = "url=http://example.com";
             var response = client.post("/urls", requestBody);
+            var createdAt = Timestamp.valueOf(LocalDateTime.now()
+                    .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+            Url url = new Url(requestBody, createdAt);
+            UrlRepository.save(url);
             assertThat(response.code()).isEqualTo(200);
             assertThat(response.body().string()).contains("http://example.com");
+            var response2 = client.get("/urls/1");
+            assertThat(response2.code()).isEqualTo(200);
+            assertThat(response2.body().string()).contains("http://example.com");
+        });
+    }
+
+    @Test
+    public void testExistingUrl() {
+        JavalinTest.test(app, (server, client) -> {
+            var requestBody = "url=http://example.com";
+            var response = client.post("/urls", requestBody);
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string()).containsOnlyOnce("http://example.com");
+        });
+    }
+
+    @Test
+    public void testBadUrl() {
+        JavalinTest.test(app, (server, client) -> {
+            var requestBody = "url=example.com";
+            var response = client.post("/urls", requestBody);
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string()).contains("Анализатор страниц");
         });
     }
 
