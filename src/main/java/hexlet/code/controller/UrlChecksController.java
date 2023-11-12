82c14cf6d76a8ed1b2c90d3d6f67418e4ac6fa0c
SickJoke282 <menistr2@DESKTOP-VVJN9FC.localdomain>
2023-11-12 20:08:32 +0500

    added a seo check for sites

--- src/main/java/hexlet/code/controller/UrlChecksController.java
+++ src/main/java/hexlet/code/controller/UrlChecksController.java
@@ -7,38 +7,37 @@ import hexlet.code.util.NamedRoutes;
 import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
 import kong.unirest.Unirest;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class UrlChecksController {
-    public static void create(Context ctx) throws SQLException {
+    public static void create(Context ctx) {
         var urlId = ctx.pathParamAsClass("id", Long.class).get();
         try {
             var url = UrlRepository.find(urlId)
                     .orElseThrow(() -> new NotFoundResponse("Entity with id = " + urlId + "not found"));
+            Document doc = Jsoup.connect(url.getName()).get();
+            Elements newsHeadlines = doc.getAllElements();
             var response = Unirest.get(url.getName()).asString();
             var statusCode = response.getStatus();
             var createdAt = Timestamp.valueOf(LocalDateTime.now()
                     .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-            var title = response.getBody().substring(
-                    response.getBody().indexOf("<title"), response.getBody().indexOf("</title>"));
-            title = title.substring(title.indexOf(">") + 1);
-            var h1 = response.getBody().substring(
-                    response.getBody().indexOf("<h1"), response.getBody().indexOf("</h1>"));
-            h1 = h1.substring(h1.indexOf(">") + 1);
-            Pattern pattern = Pattern.compile(
-                    "<meta([a-z\\s=\":]*)description([a-z\\s=\":]*)content=\"[^\\n]*$");
-            Matcher matcher = pattern.matcher(response.getBody());
-            var description = matcher.find()
-                    ? matcher.group()
-                    .substring(matcher.group()
-                            .indexOf("content=\"") + 1, matcher.group().indexOf("\">"))
-                    : "";
+            String h1 = "", title = "", description = "";
+            for (Element headline : newsHeadlines) {
+                h1 = headline.selectFirst("h1") != null
+                        ? headline.selectFirst("h1").text() : h1;
+                title = headline.selectFirst("title") != null
+                        ? headline.selectFirst("title").text() : title;
+                description = headline.selectFirst("meta[name=description]") != null
+                        ? headline.selectFirst("meta[name=description]").attr("content")
+                        : description;
+            }
             var urlCheck = new UrlCheck(statusCode, title, h1, description, createdAt);
             UrlCheckRepository.save(urlCheck, url);
             ctx.sessionAttribute("flash", "Страница успешно проверена");
