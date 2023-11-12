98c83948025fd7a6fd64f845970722842f94e233
SickJoke282 <menistr2@DESKTOP-VVJN9FC.localdomain>
2023-11-12 19:35:16 +0500

    some fixes

--- src/main/java/hexlet/code/controller/UrlChecksController.java
+++ src/main/java/hexlet/code/controller/UrlChecksController.java
@@ -8,6 +8,7 @@ import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
 import kong.unirest.Unirest;
 
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -15,7 +16,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UrlChecksController {
-    public static void create(Context ctx) {
+    public static void create(Context ctx) throws SQLException {
         var urlId = ctx.pathParamAsClass("id", Long.class).get();
         try {
             var url = UrlRepository.find(urlId)
@@ -32,11 +33,7 @@ public class UrlChecksController {
             h1 = h1.substring(h1.indexOf(">") + 1);
             Pattern pattern = Pattern.compile(
                     "<meta([a-z\\s=\":]*)description([a-z\\s=\":]*)content=\"[^\\n]*$");
-            response = Unirest.get(url.getName()).asString();
-            String str = response.getBody().substring(0, response.getBody().indexOf("<script"));
-            Matcher matcher = pattern.matcher(str);
-            System.out.println(str);
-            System.out.println(matcher.find());
+            Matcher matcher = pattern.matcher(response.getBody());
             var description = matcher.find()
                     ? matcher.group()
                     .substring(matcher.group()
